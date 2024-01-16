51f44d293211c8554f8f6e5a095c28c371ee073e
NadanaiKD <nadanai.kd@gmail.com>
2024-01-16 23:53:56 +0700

    CHNAGE: task 1 - 5

--- mnt/dags/cryptocurrency_data_pipeline.py
+++ mnt/dags/cryptocurrency_data_pipeline.py
@@ -21,26 +21,55 @@ with DAG(
     default_args=default_args,
     schedule_interval=None,
 ) as dag:
+    
+    start = DummyOperator(
+        task_id="start",
+    )
 
-    fetch_ohlcv = DummyOperator(
+    fetch_ohlcv = PythonOperator(
         task_id="fetch_ohlcv",
+        python_callalbe=_fetch_ohlcv,
     )
 
-    download_file = DummyOperator(
+    download_file = PythonOperator(
         task_id="download_file",
+        python_callalbe=_download_file,
     )
 
-    create_import_table = DummyOperator(
+    create_import_table = PostgresOperator(
         task_id="create_import_table",
+        postgres_conn_id="postgres",
+        sql="""
+            CREATE TABLE IF NOT EXISTS cryptocurrency_import (
+                timestamp BIGINT,
+                open FLOAT,
+                highest FLOAT,
+                lowers FLOAT,
+                closing FLOAT,
+                volume FLOAT
+            )
+        """,
     )
 
-    load_data_into_database = DummyOperator(
-        task_id="load_data_into_database",
-    )
+    load_data_into_database = PythonOperator(
+       task_id="load_data_into_database",
+       python_callable=_load_data_into_database,
+   )
 
-    create_final_table = DummyOperator(
-        task_id="create_final_table",
-    )
+    create_final_table = PostgresOperator(
+       task_id="create_final_table",
+       postgres_conn_id="postgres",
+       sql="""
+           CREATE TABLE IF NOT EXISTS cryptocurrency (
+               timestamp BIGINT PRIMARY KEY,
+               open FLOAT,
+               highest FLOAT,
+               lowest FLOAT,
+               closing FLOAT,
+               volume FLOAT
+           )
+       """,
+   )
 
     merge_import_into_final_table = DummyOperator(
         task_id="merge_import_into_final_table",
@@ -53,3 +82,9 @@ with DAG(
     notify = DummyOperator(
         task_id="notify",
     )
+
+    end = DummyOperator(
+        task_id="end",
+    )
+
+start >> fetch_ohlcv >> download_file >> create_import_table >> load_data_into_database >> create_final_table >> end
